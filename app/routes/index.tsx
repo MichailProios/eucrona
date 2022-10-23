d15ab9113b11e6b2b0ce6ae0bc68d98a6bb6e469
mproios <mproios@maximgrp.com>
2022-10-23 17:42:25 -0400

    changes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -346,7 +346,7 @@ export default function Index() {
         </svg>
       </Box>
 
-      <Container maxW="1200px" p={{ base: 4, sm: 10 }}>
+      <Container maxW="1200px" px={{ base: 6, md: 10 }} py={14}>
         <Stack
           direction={{ base: "column", md: "row" }}
           justifyContent="space-between"
@@ -408,28 +408,29 @@ export default function Index() {
         </Stack>
       </Container>
       <Flex
-        minH={"65vh"}
+        minH={"50vh"}
+        px={{ base: 6, md: 10 }}
+        py={14}
         align={"center"}
         justify={"center"}
-        py={12}
-        bg={useColorModeValue("gray.50", "gray.800")}
+        // bg={useColorModeValue("gray.50", "gray.800")}
       >
         <Stack
           boxShadow={"2xl"}
-          bg={useColorModeValue("white", "gray.700")}
+          bg={useColorModeValue("gray.50", "gray.700")}
           rounded={"xl"}
           p={10}
           spacing={8}
           align={"center"}
+          width="650px"
         >
-          <Icon as={NotificationIcon} w={24} h={24} />
           <Stack align={"center"} spacing={2}>
             <Heading
               textTransform={"uppercase"}
               fontSize={"3xl"}
               color={useColorModeValue("gray.800", "gray.200")}
             >
-              Subscribe
+              Keep In Touch
             </Heading>
             <Text fontSize={"lg"} color={"gray.500"}>
               Subscribe to our newsletter & stay up to date!
